29d78e0425b4754fe4caaeb8984fd1d314ee95ea
Ben Reilly <benreilly0809@gmail.com>
2024-03-10 21:16:54 -0700

    More profiles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,10 @@ export default async function Home() {
 
   return (
     <main className="container mt-4 flex min-h-screen flex-col items-center">
-      <div className="mb-12 grid grid-cols-1 justify-around gap-4 sm:grid-cols-2">
-        <p className="col-span-1 text-justify font-semibold leading-6 tracking-normal text-black sm:col-span-2 sm:tracking-widest">
-          find your favorite moments from...
-        </p>
+      <p className="col-span-1 text-justify font-semibold leading-6 tracking-normal text-black sm:col-span-2 sm:tracking-widest">
+        find your favorite moments from...
+      </p>
+      <div className="mb-12 grid grid-cols-1 justify-around gap-4 sm:grid-cols-2 md:grid-cols-3">
         {profiles.map((profile) => (
           <Profile
             key={profile.channel_id}
