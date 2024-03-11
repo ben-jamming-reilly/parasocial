0533a73e46a0997e3f6db6d2ab97ff639e83a414
Ben Reilly <benreilly0809@gmail.com>
2024-03-11 15:03:14 -0700

    prefetching b

--- src/app/Profile.tsx
+++ src/app/Profile.tsx
@@ -9,6 +9,7 @@ export interface ProfileProps {
 export default function Profile({ author, url }: ProfileProps) {
   return (
     <Link
+      prefetch
       href={`p/${author}`}
       className="relative border-4 border-black bg-black font-bold shadow-[8px_8px_0_0_#000] transition hover:shadow-none focus:outline-none focus:ring "
     >
