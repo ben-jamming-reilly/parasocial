19ed176c32aa78520eced6d509176a45d62026cc
Ben Reilly <benreilly0809@gmail.com>
2024-04-08 11:56:01 -0700

    better video scroll

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -164,42 +164,44 @@ export function Player() {
         ref={parentRef}
         className="bg-rose-900 border-black rounded-none h-[96vh]"
       >
-        <ScrollArea className="h-full overflow-auto mx-auto">
-          {video && (
-            <YouTube
-              key={`${video.id}-${start}-${end}`}
-              className="border-2 border-black bg-black"
-              videoId={getYoutubeId(video.url)}
-              id={`${video.id}-${start}-${end}-sm`}
-              loading="lazy"
-              onStateChange={onStateChange}
-              opts={{
-                width: isMobile ? 380 : width,
-                height: isMobile ? hwRatio * 380 : height,
-                playerVars: {
-                  // https://developers.google.com/youtube/player_parameters#Parameters
-                  color: "white",
-                  rel: 0,
-                  autoplay: 0,
-                  start: Number(start),
-                  controls: 1, // 0 - no controls
-                },
-              }}
-            />
-          )}
-          {start && end && video && (
-            <div
-              style={{ width: isMobile ? 380 : width }}
-              className=" flex-1 flex my-4"
-            >
-              <SimilarVideos
-                author={video.author}
-                start={start}
-                end={end}
-                videoId={video.id}
+        <ScrollArea className="h-full overflow-auto ">
+          <div className="mx-auto w-fit">
+            {video && (
+              <YouTube
+                key={`${video.id}-${start}-${end}`}
+                className="border-2 border-black bg-black"
+                videoId={getYoutubeId(video.url)}
+                id={`${video.id}-${start}-${end}-sm`}
+                loading="lazy"
+                onStateChange={onStateChange}
+                opts={{
+                  width: isMobile ? 380 : width,
+                  height: isMobile ? hwRatio * 380 : height,
+                  playerVars: {
+                    // https://developers.google.com/youtube/player_parameters#Parameters
+                    color: "white",
+                    rel: 0,
+                    autoplay: 0,
+                    start: Number(start),
+                    controls: 1, // 0 - no controls
+                  },
+                }}
               />
-            </div>
-          )}
+            )}
+            {start && end && video && (
+              <div
+                style={{ width: isMobile ? 380 : width }}
+                className=" flex-1 flex my-4"
+              >
+                <SimilarVideos
+                  author={video.author}
+                  start={start}
+                  end={end}
+                  videoId={video.id}
+                />
+              </div>
+            )}
+          </div>
         </ScrollArea>
       </DrawerContent>
     </Drawer>
